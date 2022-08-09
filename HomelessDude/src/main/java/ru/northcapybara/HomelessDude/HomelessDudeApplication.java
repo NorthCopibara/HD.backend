f71de336ec4b90638f09e86106b5f0b7b4dbf17c
NorthCopibara <ElshinAleksandr@mail.ru>
2022-08-09 15:57:45 +0300

    initial

---
+++ HomelessDude/src/main/java/ru/northcapybara/HomelessDude/HomelessDudeApplication.java
@@ -0,0 +1,13 @@
+package ru.northcapybara.HomelessDude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelessDudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelessDudeApplication.class, args);
+	}
+
+}
---
+++ HomelessDude/src/test/java/ru/northcapybara/HomelessDude/HomelessDudeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.northcapybara.HomelessDude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelessDudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
